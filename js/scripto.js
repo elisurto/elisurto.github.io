f029e4cfc697ba0e1306b10058d705ec89abb917
Elisurto <elisurto@users.noreply.github.com>
2023-04-18 21:18:08 -0600

    Functionality Adjustments

--- js/scripto.js
+++ js/scripto.js
@@ -1,20 +1,44 @@
-// Get the square element
-var squareElement = document.getElementById("square");
+var square = document.getElementById('square');
+var container = document.body;
+var xDirection = 1;
+var yDirection = 1;
 
-// Add a click event listener to the square
-squareElement.addEventListener("click", function() {
-    // Generate a random color
+square.addEventListener('click', function() {
     var randomColor = getRandomColor();
-    // Set the square background color to the random color
-    squareElement.style.backgroundColor = randomColor;
+    square.style.backgroundColor = randomColor;
 });
 
-// Function to generate a random color
 function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
+    var letters = '0123456789ABCDEF';
+    var color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
+
+function animateSquare() {
+    var left = parseInt(square.style.left) || 0;
+    var top = parseInt(square.style.top) || 0;
+    var containerWidth = container.clientWidth;
+    var containerHeight = container.clientHeight;
+    var squareWidth = square.clientWidth;
+    var squareHeight = square.clientHeight;
+
+    if (left <= 0 || left + squareWidth >= containerWidth) {
+        xDirection *= -1;
+    }
+    if (top <= 0 || top + squareHeight >= containerHeight) {
+        yDirection *= -1;
+    }
+
+    left += xDirection * 5;
+    top += yDirection * 5;
+
+    square.style.left = left + 'px';
+    square.style.top = top + 'px';
+
+    requestAnimationFrame(animateSquare);
+}
+
+animateSquare();
